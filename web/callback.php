c1ed5fa51dcd75448ccff810602414c833cbb666
Tanaka <hironari.tanaka@p40922.intra.rakuten.co.jp>
2016-12-13 15:21:51 +0900

    a

--- web/callback.php
+++ web/callback.php
@@ -26,7 +26,7 @@ if ($text == 'はい') {
       "type" => "buttons",
       "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img1.jpg",
       "title" => "○○レストラン",
-      "text" => "お探しのレストランはこれですね",
+      "text" => "君は何が欲しいんだ",
       "actions" => [
           [
             "type" => "postback",
