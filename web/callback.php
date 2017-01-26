43409178951eaeb38ba1618bc114504a4105fde4
Tanaka <hironari.tanaka@p40922.intra.rakuten.co.jp>
2017-01-26 16:10:58 +0900

    a

--- web/callback.php
+++ web/callback.php
@@ -68,7 +68,7 @@ else if ($text == 'ランキング') {
               [
                   "type" => "uri",
                   "label" => "購入する",
-                  "uri" => "http://video.rakuten.co.jp/content/177408/"
+                  "uri" => ""
               ]
             ]
           ],
@@ -78,19 +78,19 @@ else if ($text == 'ランキング') {
             "text" => "価格：432円〜",
             "actions" => [
               [
-                  "type" => "uri",
+                  "type" => "postback",
                   "label" => "告知を見る",
-                  "uri" => "http://video.rakuten.co.jp/content/166188/?l-id=cpn_201611rank_top5_03"
+                  "data" => "action=rsv&itemid=222"
               ],
               [
-                  "type" => "uri",
+                  "type" => "postback",
                   "label" => "Webで詳細を見る",
-                  "uri" => "http://video.rakuten.co.jp/content/166188/?l-id=cpn_201611rank_top5_03"
+                  "data" => "action=pcall&itemid=222"
               ],
               [
                   "type" => "uri",
                   "label" => "購入する",
-                  "uri" => "http://video.rakuten.co.jp/basket/add/293958/"
+                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
               ]
             ]
           ],
@@ -100,19 +100,19 @@ else if ($text == 'ランキング') {
             "text" => "価格：432円〜",
             "actions" => [
               [
-                  "type" => "uri",
+                  "type" => "postback",
                   "label" => "告知を見る",
-                  "uri" => "http://video.rakuten.co.jp/content/177408/"
+                  "data" => "action=rsv&itemid=222"
               ],
               [
-                  "type" => "uri",
+                  "type" => "postback",
                   "label" => "Webで詳細を見る",
-                  "uri" => "action=pcall&itemid=222"
+                  "data" => "action=pcall&itemid=222"
               ],
               [
                   "type" => "uri",
                   "label" => "購入する",
-                  "uri" => "http://video.rakuten.co.jp/basket/add/299721/"
+                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
               ]
             ]
           ],
